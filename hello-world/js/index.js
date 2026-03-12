81c0f078799f20d80fcbdb4fe2d42697f3ada55c
Yan Yoeurn <yoeurn.yan.dev@gmail.com>
2026-03-12 23:01:31 +0700

    update lesson3

--- hello-world/js/index.js
+++
@@ -1,7 +0,0 @@
-// My First JavaScript!
-console.log("Hello World! My website is working!");
-
-// Display current date and time
-const now = new Date();
-console.log("Current Date:", now.toLocaleDateString());
-console.log("Current Time:", now.toLocaleTimeString());
