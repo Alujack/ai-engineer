db135dab49c0e6d6c9cec550d9d4b44184b5cccc
Yan Yoeurn <yoeurn.yan.dev@gmail.com>
2026-06-08 23:35:16 +0700

    update the lesson

--- teachers/demo-app/src/main.jsx
+++ teachers/demo-app/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Students from './students.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Students />
   </StrictMode>,
 )
---
+++ teachers/demo-app/src/students.jsx
@@ -0,0 +1,11 @@
+import "react"
+export default function Students() {
+  return (
+    <>
+      <h1>Students</h1>
+      <p>This is the students page. her name is Malen</p>
+      <p>She is a good students</p>
+      <p>She has a good boyfriend</p>
+    </>
+  )
+}
